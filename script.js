de2e865501c645cc8e00465d916a1305a5b96596
Sanjay R <77227213+SanJay1577@users.noreply.github.com>
2021-01-29 06:17:15 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ request.send();
 request.onload = function () { 
     var data = JSON.parse(this.response);
     var region = data.filter((reg) => {
-         return reg.population <200000;
+         return reg.currencies = Dollar;
     }).map((ele) => {
             return ele.name})
         console.log(region);
